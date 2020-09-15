d76bbcdc2eda4f92f5ed270a4b6ae84735013f91
yanghaodong6295 <yanghaodong6295@ipalfish.com>
2020-09-15 20:19:38 +0800

    fix build

--- build.py
+++ build.py
@@ -3,15 +3,22 @@ import os
 import sys
 import argparse
 
-def main(input_data_path,output_data_path):
+def main(input_data_path):
     cmd='GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/multi_hand_tracking/multi_hand_tracking_cpu_text \--calculator_graph_config_file=mediapipe/graphs/hand_tracking/multi_hand_tracking_desktop_live_text.pbtxt'
     
     listfile=os.listdir(input_data_path)
     
     for file in listfile:
+    
+        if ".DS_Store" in file:
+           continue
+           
         fullfilename=os.listdir(input_data_path+file)
         for mp4list in fullfilename:
-            inputfilen='   --input_video_path='+fullfilename
+            if ".DS_Store" in mp4list or "txt" in mp4list:
+                    continue
+            inputfilen='   --input_video_path='+input_data_path+file+'/'+mp4list
+            print(mp4list)
             cmdret=cmd+inputfilen
             os.system(cmdret)
 
@@ -21,4 +28,4 @@ if __name__ == "__main__":
     args=parser.parse_args()
     input_data_path=args.input_data_path
     print(input_data_path)
-    main(input_data_path,output_data_path)
+    main(input_data_path)
